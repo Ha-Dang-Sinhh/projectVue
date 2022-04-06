bb1c7c096301b565985f7a9878d70fa292efd437
Sinh <hadangsinh1911@gmail.com>
2022-04-06 22:33:28 +0700

    add list cart product

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <ListProducts></ListProducts> -->
+    <CartProducts></CartProducts>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import ListProducts from './components/ListProducts.vue'
+import CartProducts from './components/CartProducts.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // ListProducts,
+    CartProducts,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 0;
+  margin:0;
+  box-sizing: border-box;
 }
 </style>
